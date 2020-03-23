f946e6bc6d93f48b93ae2c63ef8b8a175ec702ac
Budi Rahardjo <rahard@gmail.com>
2020-03-23 12:09:48 +0700

    update contoh-parse.py

--- queue/contoh-parse.py
+++ queue/contoh-parse.py
@@ -4,10 +4,14 @@ import sys
 name = sys.argv[0]
 numberargs = len(sys.argv)
 if (numberargs) < 4:
-   print("Usage: " + name + "IDpenyetor root follower")
+   print("Usage: " + name + " IDpenyetor root follower")
    exit(1)
 
 # get data
+IDpenyetor = sys.argv[1]
 root = sys.argv[2]
 follower = sys.argv[3]
-print("Kirim " + root + " " + follower)
+pesan= IDpenyetor + " " + root + " " + follower
+print("Kirim " + pesan)
+
+# eksekusi pengiriman
